a70d2c060bff743103da696ec64900beec5bc093
Paul Git <paulgit@pm.me>
2023-04-03 21:02:57 +0800

    Refactored to use dnsmasq rules for Apple, Google etc. Removed IP list code and rely on Geo database in Shadowrocket

--- build.sh
+++ build.sh
@@ -9,11 +9,8 @@ BUILD_TIME="${YEAR}-${MONTH}-${DAY} $TIME"
 SCRIPT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD_FOLDER="$SCRIPT_FOLDER/build"
 REGEX_DOMAIN="\.?\b([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b"
-ACCEL_DOMAIN_FILES="$SCRIPT_FOLDER/dnsmasq-china-list/accelerated-domains.china.conf"
-IPCIDR_FILES="$SCRIPT_FOLDER/chn-iplist/chnroute-ipv4.txt"
+ACCEL_DOMAIN_FILES="$SCRIPT_FOLDER/dnsmasq-china-list/apple.china.conf $SCRIPT_FOLDER/dnsmasq-china-list/google.china.conf $SCRIPT_FOLDER/dnsmasq-china-list/accelerated-domains.china.conf"
 SHADOWROCKET_CONF="$SCRIPT_FOLDER/build/china.conf"
-APPLE_RULES_FILE=$SCRIPT_FOLDER/templates/apple-rules.template
-GOOGLE_RULES_FILE=$SCRIPT_FOLDER/templates/google-rules.template
 OTHER_RULES_FILE=$SCRIPT_FOLDER/templates/other-rules.template
 
 printf "Building Paul Git's Shadowrocket Rules for China\n"
@@ -29,7 +26,6 @@ git submodule foreach git reset --hard HEAD
 git submodule foreach git clean -df 
 git submodule foreach git pull origin master --no-rebase
 
-
 printf "# Paul Git's Shadowrocket Rules for China\n" > $SHADOWROCKET_CONF
 printf "#   https://code.paulg.it/shadowrocket-china\n" >> $SHADOWROCKET_CONF
 printf "#\n" >> $SHADOWROCKET_CONF
@@ -40,26 +36,22 @@ printf "# Build Time: $BUILD_TIME\n" >> $SHADOWROCKET_CONF
 printf "\n" >> $SHADOWROCKET_CONF
 printf "[General]\n" >> $SHADOWROCKET_CONF
 printf "bypass-system = true\n" >> $SHADOWROCKET_CONF
-printf "dns-server = 114.114.114.114, 119.29.29.29, 1.1.1.1, 208.67.222.222, 8.8.8.8\n" >> $SHADOWROCKET_CONF
-printf "skip-proxy = 127.0.0.1, 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, 100.64.0.0/10, 17.0.0.0/8, localhost, *.local, *.lan, *.crashlytics.com\n" >> $SHADOWROCKET_CONF
-printf "bypass-tun = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.18.0.0/15, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32\n" >> $SHADOWROCKET_CONF
+printf "skip-proxy = 192.168.0.0/16, 10.0.0.0/8, 172.16.0.0/12, localhost, *.local, captive.apple.com\n" >> $SHADOWROCKET_CONF
+printf "tun-excluded-routes = 10.0.0.0/8, 100.64.0.0/10, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 255.255.255.255/32, 239.255.255.250/32\n" >> $SHADOWROCKET_CONF
+printf "dns-server = system\n" >> $SHADOWROCKET_CONF
+printf "ipv6 = true\n" >> $SHADOWROCKET_CONF
+printf "prefer-ipv6 = false\n" >> $SHADOWROCKET_CONF
+printf "dns-fallback-system = false\n" >> $SHADOWROCKET_CONF
+printf "dns-direct-system = false\n" >> $SHADOWROCKET_CONF
+printf "icmp-auto-reply = true\n" >> $SHADOWROCKET_CONF
+printf "always-reject-url-rewrite = false\n" >> $SHADOWROCKET_CONF
+printf "private-ip-answer = true\n" >> $SHADOWROCKET_CONF
+printf "# direct domain fail to resolve use proxy rule\n" >> $SHADOWROCKET_CONF
+printf "dns-direct-fallback-proxy = true\n" >> $SHADOWROCKET_CONF
 printf "\n" >> $SHADOWROCKET_CONF
 printf "[Rule]\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,1.1.1.1/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,1.0.0.1/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,8.8.8.8/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,8.8.4.4/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,9.9.9.9/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,208.67.222.222/32,PROXY\n" >> $SHADOWROCKET_CONF
-printf "IP-CIDR,149.112.112.112/32,PROXY\n" >> $SHADOWROCKET_CONF
 printf "DOMAIN-SUFFIX,cn,DIRECT\n" >> $SHADOWROCKET_CONF
-
-printf "## > Apple Service Rules for China\n" >> $SHADOWROCKET_CONF
-cat $APPLE_RULES_FILE >> $SHADOWROCKET_CONF
-printf "\n\n" >> $SHADOWROCKET_CONF
-printf "## > Google Service Rules for China\n" >> $SHADOWROCKET_CONF
-cat $GOOGLE_RULES_FILE >> $SHADOWROCKET_CONF
-printf "\n\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
 printf "## > Accelerated Domain Rules for China\n" >> $SHADOWROCKET_CONF
 for FILE in ${ACCEL_DOMAIN_FILES}; do
   printf "\nAdding $FILE...\n"
@@ -69,20 +61,24 @@ for FILE in ${ACCEL_DOMAIN_FILES}; do
     printf "DOMAIN-SUFFIX,$DOMAIN_ENTRY,DIRECT\n" >> $SHADOWROCKET_CONF
   done
 done
-printf "\n\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
 printf "## > Paul Git's Other Rules for China\n" >> $SHADOWROCKET_CONF
 cat $OTHER_RULES_FILE >> $SHADOWROCKET_CONF
 printf "\n\n" >> $SHADOWROCKET_CONF
-
-printf "## > IPCIDR list for China\n" >> $SHADOWROCKET_CONF
-for FILE in ${IPCIDR_FILES}; do
-  printf "\nAdding $FILE...\n"
-  IPCIDR_LIST=$(grep -v '^#' "$FILE")
-  for IPCIDR_ENTRY in $IPCIDR_LIST
-  do
-    printf "IP-CIDR,$IPCIDR_ENTRY,DIRECT\n" >> $SHADOWROCKET_CONF
-  done
-done
-printf "\n\n" >> $SHADOWROCKET_CONF
-
-printf "FINAL,PROXY" >> $SHADOWROCKET_CONF
+printf "# LAN\n" >> $SHADOWROCKET_CONF
+printf "IP-CIDR,192.168.0.0/16,DIRECT\n" >> $SHADOWROCKET_CONF
+printf "IP-CIDR,10.0.0.0/8,DIRECT\n" >> $SHADOWROCKET_CONF
+printf "IP-CIDR,172.16.0.0/12,DIRECT\n" >> $SHADOWROCKET_CONF
+printf "IP-CIDR,127.0.0.0/8,DIRECT\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
+printf "# China\n" >> $SHADOWROCKET_CONF
+printf "GEOIP,CN,DIRECT\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
+printf "FINAL,PROXY\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
+printf "[Host]\n" >> $SHADOWROCKET_CONF
+printf "localhost = 127.0.0.1\n" >> $SHADOWROCKET_CONF
+printf "\n" >> $SHADOWROCKET_CONF
+printf "[URL Rewrite]\n" >> $SHADOWROCKET_CONF
+printf "^https\?://(www.)\?g.cn https://www.google.com 302\n" >> $SHADOWROCKET_CONF
+printf "^https\?://(www.)\?google.cn https://www.google.com 302\n" >> $SHADOWROCKET_CONF
